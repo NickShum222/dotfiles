feff04981c89e0fd471fa4f9be982a743bb43813
Nick Shum <n4shum@uwaterloo.ca>
2025-05-24 11:07:02 -0400

    Added nvim & raycast

---
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,34 @@
+-- ~/.config/nvim/lua/plugins/colorscheme.lua
+return {
+  {
+    "rebelot/kanagawa.nvim",
+    priority = 1000, -- Make sure it's loaded before other plugins
+    config = function()
+      require("kanagawa").setup({
+        compile = false,
+        undercurl = true,
+        commentStyle = { italic = true },
+        functionStyle = {},
+        keywordStyle = { italic = true },
+        statementStyle = { bold = true },
+        typeStyle = {},
+        transparent = false,
+        dimInactive = false,
+        terminalColors = true,
+        colors = {
+          palette = {},
+          theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+        },
+        overrides = function(colors)
+          return {}
+        end,
+        theme = "wave",
+        background = {
+          dark = "wave",
+          light = "lotus",
+        },
+      })
+      vim.cmd("colorscheme kanagawa-dragon")
+    end,
+  },
+}
