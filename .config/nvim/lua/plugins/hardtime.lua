00316c6393ffd45e6fc9417144a2c5e76ada408c
Nick Shum <n4shum@uwaterloo.ca>
2025-05-29 15:24:52 -0400

    update

--- .config/nvim/lua/plugins/hardtime.lua
+++ .config/nvim/lua/plugins/hardtime.lua
@@ -2,5 +2,13 @@ return {
   "m4xshen/hardtime.nvim",
   lazy = false,
   dependencies = { "MunifTanjim/nui.nvim" },
-  opts = { disable_mouse = true },
+  opts = {
+    disable_mouse = true,
+    disabled_keys = {
+      ["<Up>"] = { "n", "x" }, -- Allow <Up> key
+      ["<Down>"] = { "n", "x" },
+      ["<Left>"] = { "n", "x" },
+      ["<Right>"] = { "n", "x" },
+    },
+  },
 }
