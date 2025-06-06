e08878e5893d1a20e4990c42b86b26fe371cbfaa
NickShum <115332882+NickShum222@users.noreply.github.com>
2025-06-06 01:02:03 -0400

    update git alias

--- .config/nvim/lua/plugins/hardtime.lua
+++ .config/nvim/lua/plugins/hardtime.lua
@@ -1,14 +1,14 @@
--- return {
--- 	"m4xshen/hardtime.nvim",
--- 	event = "VeryLazy",
--- 	dependencies = { "MunifTanjim/nui.nvim" },
--- 	opts = {
--- 		disable_mouse = true,
--- 		disabled_keys = {
--- 			["<Up>"] = { "n", "x" }, -- Allow <Up> key
--- 			["<Down>"] = { "n", "x" },
--- 			["<Left>"] = { "n", "x" },
--- 			["<Right>"] = { "n", "x" },
--- 		},
--- 	},
--- }
+return {
+	-- 	"m4xshen/hardtime.nvim",
+	-- 	event = "VeryLazy",
+	-- 	dependencies = { "MunifTanjim/nui.nvim" },
+	-- 	opts = {
+	-- 		disable_mouse = true,
+	-- 		disabled_keys = {
+	-- 			["<Up>"] = { "n", "x" }, -- Allow <Up> key
+	-- 			["<Down>"] = { "n", "x" },
+	-- 			["<Left>"] = { "n", "x" },
+	-- 			["<Right>"] = { "n", "x" },
+	-- 		},
+	-- 	},
+}
