2c246a897ed22568fe93d83f8cf81469f59e7488
NICK SHUM <n4shum@uwaterloo.ca>
2025-10-14 13:02:10 -0400

    moved plugins to archive

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -4,9 +4,10 @@ return {
 	---@type oil.SetupOpts
 	opts = {},
 	-- Optional dependencies
-	dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+	dependencies = { "nvim-tree/nvim-web-devicons", "benomahony/oil-git.nvim" }, -- use if you prefer nvim-web-devicons
 	lazy = false,
 	vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory in Oil" }),
+	vim.keymap.set("n", "<leader>.", "<CMD>OilToggleHidden<CR>", { desc = "Toggle hidden files" }),
 	config = function()
 		require("oil").setup({
 			view_options = { show_hidden = true },
