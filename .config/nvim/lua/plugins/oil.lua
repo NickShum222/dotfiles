acb71d359e244ce0f30440bbc2c1473cd40d03a0
NICK SHUM <n4shum@uwaterloo.ca>
2025-10-15 16:43:27 -0400

    karabiner, disable esc & arrow keys

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -7,7 +7,7 @@ return {
 	dependencies = { "nvim-tree/nvim-web-devicons", "benomahony/oil-git.nvim" }, -- use if you prefer nvim-web-devicons
 	lazy = false,
 	vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory in Oil" }),
-	vim.keymap.set("n", "<leader>.", "<CMD>OilToggleHidden<CR>", { desc = "Toggle hidden files" }),
+	-- vim.keymap.set("n", "<leader>.", "<CMD>OilToggleHidden<CR>", { desc = "Toggle hidden files" }),
 	config = function()
 		require("oil").setup({
 			view_options = { show_hidden = true },
