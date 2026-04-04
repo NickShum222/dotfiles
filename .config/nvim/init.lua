235af025af027dd105dc16abcb67160bc2012f4e
NICK SHUM <n4shum@uwaterloo.ca>
2026-04-04 04:33:27 -0400

    nvim init

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,1 +1,61 @@
-require("config")
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- Keybinds
+vim.keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
+vim.keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
+vim.keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
+
+vim.o.relativenumber = true
+vim.o.number = true
+vim.o.cursorline = true
+
+vim.o.tabstop = 2 -- 2 spaces for tabs (prettier default)
+vim.o.shiftwidth = 2 -- 2 spaces for indent width
+vim.o.expandtab = true -- expand tab to spaces
+vim.o.smartindent = true
+vim.o.linebreak = true
+
+vim.o.ignorecase = true -- ignore case when searching
+vim.o.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
+
+vim.o.termguicolors = true
+
+vim.opt.clipboard:append("unnamedplus") -- use system clipboard as default register
+
+vim.o.confirm = true
+
+vim.o.undofile = true
+vim.o.undodir = vim.fn.stdpath("state") .. "/undo"
+
+require("lazy").setup({
+	{
+		"stevearc/oil.nvim",
+		lazy = false,
+		vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory in Oil" }),
+		config = function()
+			require("oil").setup({
+				view_options = { show_hidden = true },
+			})
+		end,
+	},
+})
