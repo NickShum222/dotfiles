9d6339750a32dced5b2e7d3b8cd2ba8d34437058
NICK SHUM <n4shum@uwaterloo.ca>
2026-04-08 20:57:37 -0400

    update treesitter for nvim 0.12

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -205,17 +205,35 @@ require("lazy").setup({
 	{
 		"nvim-treesitter/nvim-treesitter",
 		event = { "BufReadPre", "BufNewFile" },
-		branch = "master",
+		branch = "main",
 		lazy = false,
 		build = ":TSUpdate",
 		dependencies = {
 			"windwp/nvim-ts-autotag",
 		},
-		config = function()
-			require("nvim-treesitter.configs").setup({
-				ensure_installed = { "c", "cpp", "python", "javascript", "typescript", "tsx" },
-				highlight = { enable = true },
-				indent = { enable = true },
+		init = function()
+			local ensure_installed = {
+				"c",
+				"cpp",
+				"python",
+				"javascript",
+				"typescript",
+			}
+
+			local alreadyInstalled = require("nvim-treesitter.config").get_installed()
+			local parsersToInstall = vim.iter(ensure_installed)
+				:filter(function(parser)
+					return not vim.tbl_contains(alreadyInstalled, parser)
+				end)
+				:totable()
+
+			require("nvim-treesitter").install(parsersToInstall)
+
+			vim.api.nvim_create_autocmd("FileType", {
+				callback = function()
+					pcall(vim.treesitter.start)
+					vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+				end,
 			})
 			vim.treesitter.language.register("tsx", "typescriptreact")
 			require("nvim-ts-autotag").setup()
